bc2c39c50775daff6a7039c1ead2c24090447ce8
Yash Patel <yash.pateltx@gmail.com>
2025-10-23 17:01:40 -0500

    Inital commit: added skeleton correctly

--- dijkstra.py
+++ dijkstra.py
@@ -1,3 +1,23 @@
 import heapq
 
-def dijk
+def dijkstra(graph, source):
+    dist  = {u: float('inf') for u in graph}
+    parent = {u: None for u in graph}
+    dist[source] = 0
+    #implement here
+    return dist, parent
+
+def show_path(parent, target):
+    path = []
+    #implement path reconstruction
+    return path
+
+if __name__ == "__main__":
+    graph = {
+        #add edges
+    }
+
+    source = 'A'
+    dist, parent = dijkstra(graph, source)
+    target = 'D'
+    #print distances and path
